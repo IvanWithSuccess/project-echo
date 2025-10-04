c2a91d3976646b423403e1b6979842e65f2395ed
delaukr <delaukr@gmail.com>
2025-10-04 18:00:13 +0000

    fix

--- project_echo/app.py
+++ project_echo/app.py
@@ -1,6 +1,5 @@
 
 import toga
-import asyncio
 from toga.style import Pack
 from toga.style.pack import COLUMN
 
@@ -11,23 +10,28 @@ from project_echo.views.ad_cabinet_view import AdCabinetView
 class ProjectEcho(toga.App):
 
     def startup(self):
+        # 1. Create the main window.
         self.main_window = toga.MainWindow(title=self.formal_name, size=(800, 600))
-        self.option_container = toga.OptionContainer(style=Pack(flex=1))
-        self.main_window.content = self.option_container
-        self.main_window.show()
 
-        # Use asyncio.create_task as recommended
-        asyncio.create_task(self.add_tabs())
-
-    async def add_tabs(self):
-        # A short delay can sometimes help with UI initialization.
-        await asyncio.sleep(0.05)
-        
+        # 2. Create the views that will be the content of the tabs *first*.
         account_manager_view = AccountView(self)
         ad_cabinet_view = AdCabinetView(self)
 
-        self.option_container.add('Accounts', account_manager_view)
-        self.option_container.add('Ad Cabinet', ad_cabinet_view)
+        # 3. Create the OptionContainer and provide the tabs directly
+        #    to the 'content' argument during initialization.
+        option_container = toga.OptionContainer(
+            style=Pack(flex=1),
+            content=[
+                ('Accounts', account_manager_view),
+                ('Ad Cabinet', ad_cabinet_view)
+            ]
+        )
+
+        # 4. Set the fully-constructed container as the content of the window.
+        self.main_window.content = option_container
+
+        # 5. Show the window.
+        self.main_window.show()
 
 def main():
     return ProjectEcho('Project Echo', 'org.beeware.project_echo')
